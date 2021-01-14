e3f108fc63adc5adefd8ed6fa54f84649efe5699
Brendon Mota <bmota@its.jnj.com>
2021-01-14 12:37:54 -0300

    Product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,7 +3,7 @@ import '../styles/product-card.css';
 function ProductCard() {
   return (
     <div className="card card-body">
-      <img className="img-fluid" src='https://react-shooping-cart.netlify.app/img/1.jpg?v=1' alt="Nome da Imagem" />
+      <img className="img-fluid" src='/img/product-test.jpg' alt="Nome da Imagem" />
       <p>Nome do Produto</p>
       <h3 className="text-left">R$ 100,00</h3>
       <div className="text-right">
