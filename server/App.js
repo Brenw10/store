cf23542199a786d005a5c20c886a17f7b0a62d4c
Brendon Mota <bmota@its.jnj.com>
2021-02-23 21:35:34 -0300

    Enabling public foler and json limit

--- server/App.js
+++ server/App.js
@@ -9,7 +9,8 @@ const app = express();
 const port = process.env.SERVER_PORT;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
+app.use('/public', express.static(__dirname + '/public'));
 
 app.use('/product', Product);
 app.use('/category', Category);
