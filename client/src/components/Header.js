c0fae1246b045b2fdc1290ba644a8ce3e9775386
Brendon Mota <bmota@its.jnj.com>
2021-03-08 14:08:16 -0300

    Improving header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import '../styles/header.css'
 import { useCart } from '../contexts/Cart';
 
 function Header() {
 	const { getAllBuy } = useCart();
 
 	return (
-		<header>
-			<Link to='/'>Loja</Link>
-			<Link to='/about'>Sobre</Link>
-			<Link to='/cart'>Carrinho ({getAllBuy()})</Link>
+		<header className="text-center m-3">
+			<Link to='/' className="text-decoration-none text-dark mx-3">
+				Loja
+			</Link>
+			<Link to='/about' className="text-decoration-none text-dark mx-3">
+				Sobre
+			</Link>
+			<Link to='/cart' className="text-decoration-none text-dark mx-3">
+				Carrinho ({getAllBuy()})
+			</Link>
 		</header>
 	);
 }
