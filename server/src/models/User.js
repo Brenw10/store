f75eadd4b639acafddfad8cbd5b29c84bc254b98
Brendon Mota <bmota@its.jnj.com>
2021-03-09 19:54:51 -0300

    Admin flag for Users

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -7,6 +7,7 @@ const Schema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   image: { type: String, required: true },
+  admin: { type: Boolean, required: true, default: false },
 });
 
 module.exports = mongoose.model(Collection, Schema, Collection);
