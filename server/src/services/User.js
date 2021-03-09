6c45205be8917ec9b1643ada2d8520a3ddcde7a9
Brendon Mota <bmota@its.jnj.com>
2021-03-09 20:09:01 -0300

    User Entity functions

--- server/src/services/User.js
+++ server/src/services/User.js
@@ -4,6 +4,16 @@ function create(authId, user) {
   return User.updateOne({ authId }, user, { upsert: true });
 }
 
+function getByAuthId(authId) {
+  return User.findOne({ authId });
+}
+
+function isAdmin(authId) {
+  return getByAuthId(authId).then(user => user.isAdmin);
+}
+
 module.exports = {
   create,
+  getByAuthId,
+  isAdmin,
 };
