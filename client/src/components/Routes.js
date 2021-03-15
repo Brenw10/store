519cbe7dc6aa4a0fb22795cdfa503b20dfe1b74d
Brendon Mota <bmota@its.jnj.com>
2021-03-15 20:49:07 -0300

    User Login in all routes

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -3,17 +3,37 @@ import About from "../pages/About";
 import Cart from "../pages/Cart";
 import Store from "../pages/Store";
 import Product from "../pages/Product";
+import GoogleLogin from 'react-google-login';
+import { GOOGLE_AUTH } from '../constants/Api';
+import User from '../services/User';
+import { useUser } from '../contexts/User';
 
 function Routes() {
+	const { user, setUser } = useUser();
+
+	function onSignIn({ tokenId }) {
+		User
+			.get(tokenId)
+			.then(({ data }) => setUser({ ...data, tokenId }));
+	}
+
+	function renderLogin() {
+		return <GoogleLogin className="position-fixed float-right-button" onSuccess={onSignIn}
+			clientId={GOOGLE_AUTH} buttonText="" cookiePolicy={'single_host_origin'} />;
+	}
+
 	return (
-		<Router>
-			<Switch>
-				<Route exact path="/" component={Store} />
-				<Route exact path="/cart" component={Cart} />
-				<Route exact path="/about" component={About} />
-				<Route exact path="/product/:id" component={Product} />
-			</Switch>
-		</Router>
+		<>
+			<Router>
+				<Switch>
+					<Route exact path="/" component={Store} />
+					<Route exact path="/cart" component={Cart} />
+					<Route exact path="/about" component={About} />
+					<Route exact path="/product/:id" component={Product} />
+				</Switch>
+			</Router>
+			{!user && renderLogin()}
+		</>
 	);
 }
 
--- client/src/pages/Store.js
+++ client/src/pages/Store.js
@@ -7,24 +7,14 @@ import Logo from "../components/Logo";
 import '../styles/store.css';
 import { useState } from "react";
 import Modal from 'react-modal';
-import GoogleLogin from 'react-google-login';
-import { GOOGLE_AUTH } from '../constants/Api';
 import { useUser } from '../contexts/User';
-import User from '../services/User';
 
 function Store() {
 	const [category, setCategory] = useState();
 	const [modal, setModal] = useState();
-	const { user, setUser } = useUser();
-
-	function onSignIn({ tokenId }) {
-		User
-			.get(tokenId)
-			.then(({ data }) => setUser({ ...data, tokenId }));
-	}
+	const { user } = useUser();
 
 	function renderAdmin() {
-		if (!user.isAdmin) return;
 		return (
 			<button type="button" onClick={() => setModal(true)}
 				className="btn btn-danger rounded-circle position-fixed float-right-button m-3">+</button>
@@ -49,14 +39,7 @@ function Store() {
 				</div>
 			</div>
 			<Footer />
-			{
-				!user
-					?
-					<GoogleLogin className="position-fixed float-right-button" onSuccess={onSignIn}
-						clientId={GOOGLE_AUTH} buttonText="" cookiePolicy={'single_host_origin'} />
-					:
-					renderAdmin()
-			}
+			{ user && user.isAdmin && renderAdmin()}
 			<Modal isOpen={modal} style={customStyles}>
 				<button type="button"
 					onClick={() => setModal(false)}
