85a8eb8c3d1d6e469e282ac54d8895cbf8d394ce
Brendon Mota <bmota@its.jnj.com>
2021-03-23 18:57:41 -0300

    Created LoginButton component

---
+++ client/src/components/LoginButton.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import { GOOGLE_AUTH } from '../constants/Api';
+import User from '../services/User';
+import { useUser } from '../contexts/User';
+
+function LoginButton() {
+  const { setUser } = useUser();
+
+  async function onSignIn({ tokenId }) {
+    User.create(tokenId)
+      .then(() => User.get(tokenId))
+      .then(({ data }) => setUser({ ...data, tokenId }));
+  }
+
+  return (
+    <GoogleLogin clientId={GOOGLE_AUTH} onSuccess={onSignIn} cookiePolicy={'single_host_origin'}
+      render={renderProps =>
+        <button type="button" disabled={renderProps.disabled}
+          onClick={renderProps.onClick} className="btn">FAZER LOGIN</button>
+      }
+    />
+  );
+}
+
+export default LoginButton;
