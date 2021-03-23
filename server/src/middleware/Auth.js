947c1d09184110e02f2fb648d7aacf7e0de795a0
Brendon Mota <bmota@its.jnj.com>
2021-03-23 18:31:53 -0300

    Removed unused try catch

--- server/src/middleware/Auth.js
+++ server/src/middleware/Auth.js
@@ -4,15 +4,11 @@ const Auth = require('../services/Auth');
 const router = express.Router();
 
 router.use(async function (req, res, next) {
-  try {
-    const user = await Auth.getUser(req.headers.authorization);
-    if (user.sub) {
-      res.locals.user = user;
-      next();
-    } else {
-      res.sendStatus(401);
-    }
-  } catch {
+  const user = await Auth.getUser(req.headers.authorization);
+  if (user.sub) {
+    res.locals.user = user;
+    next();
+  } else {
     res.sendStatus(401);
   }
 });
