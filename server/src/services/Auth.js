cce1e11cbe05e259fecdad507a21adccfa0a5528
Brendon Mota <bmota@its.jnj.com>
2021-03-23 18:32:15 -0300

    auth parameter changed naming

--- server/src/services/Auth.js
+++ server/src/services/Auth.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
 const GOOGLE_API = require('../core/Auth');
 
-function getUser(id_token) {
+function getUser(token) {
   return axios
-    .get(GOOGLE_API, { params: { id_token } })
+    .get(GOOGLE_API, { params: { id_token: token } })
     .then(response => response.data);
 }
 
