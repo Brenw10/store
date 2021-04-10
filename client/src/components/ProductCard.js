4fa83c1c6f0afe5062d95354fb7e0d8e694aa267
Brendon Mota <bmota@its.jnj.com>
2021-04-10 17:11:13 -0300

    Improving ProductCard Layout - image at top, text at bottom

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -4,10 +4,12 @@ import { ENDPOINT } from '../constants/Api';
 function ProductCard({ product }) {
   return (
     <div className="card h-100">
-      <div className="card-body d-flex flex-column justify-content-end text-center">
+      <div className="card-body d-flex flex-column text-center">
         <img className="img-fluid mb-3" width="100%" src={ENDPOINT + product.images[0]} alt={product.name} />
-        <p>{product.name}</p>
-        <h3>R$ {Number(product.price).toLocaleString()}</h3>
+        <div className="h-100 d-flex flex-column justify-content-end">
+          <p>{product.name}</p>
+          <h3>R$ {Number(product.price).toLocaleString()}</h3>
+        </div>
       </div>
     </div>
   );
